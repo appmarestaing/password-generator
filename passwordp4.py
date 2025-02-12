18f052224d44049356f64a8df2b2624e05c07fae
appmarestaing marestaing <marestaing.noah@pusd.us>
2025-02-12 19:23:14 +0000

    updated words

--- passwordp4.py
+++ passwordp4.py
@@ -24,17 +24,20 @@ def fetch_word():
     word = response.json()[0] 
     return word
 
-def replaceLetters():
+def replaceLetters(word):
     word = word[0].upper() + word[1:]
 
     if "a" in word:
         word = word.replace("a", "@")
+    # replace at least 3 other letters 
 
     return word 
 
 def generate_weaker_password():
     word1 = fetch_word()
     word2 = fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     password = word1 + word2
     return password
 
